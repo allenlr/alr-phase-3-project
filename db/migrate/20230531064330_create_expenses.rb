07a85522782075d78a69bb8625f3619fe0721b62
allenlr <allenlru55@gmail.com>
2023-05-31 01:46:34 -0500

    correct migration errors and migrate new tables

--- db/migrate/20230531064330_create_expenses.rb
+++ db/migrate/20230531064330_create_expenses.rb
@@ -2,7 +2,7 @@ class CreateExpenses < ActiveRecord::Migration[6.1]
   def change
     create_table :expenses do |t|
       t.string :name
-      t.decimal :amount, precision :8, scale :2
+      t.decimal :amount, precision: 8, scale: 2
       t.date :date_incurred
       t.string :category
       t.string :payment_method
